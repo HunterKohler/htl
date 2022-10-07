466ff02f9af38ce795b61a0006a7aff792e58827
John Hunter Kohler <jhunterkohler@gmail.com>
2022-10-07 10:50:16 -0400

    remove unnessesary concepts and add documentation

--- htl/concepts.h
+++ htl/concepts.h
@@ -15,35 +15,15 @@
 namespace htl {
 
 /**
- * See: https://en.cppreference.com/w/cpp/concepts/boolean-testable
+ * Specifies a boolean-testable type. Specifically, that it the type `T`, and
+ * its logical inverse are convertible to `bool`.
+ *
+ * @see https://en.cppreference.com/w/cpp/concepts/boolean-testable
  */
 template <class T>
 concept BooleanTestable =
     std::convertible_to<T, bool> &&
     requires (T &&t) { { !std::forward<T>(t) } -> std::convertible_to<bool>; };
-
-template <class T>
-concept Arithmetic = std::is_arithmetic_v<T>;
-
-template <class T>
-concept Nullable =
-    std::regular<T> &&
-    std::equality_comparable_with<T, std::nullptr_t> &&
-    std::assignable_from<T, std::nullptr_t> &&
-    std::constructible_from<T, std::nullptr_t>;
-
-template <class T>
-concept NullablePointer = BooleanTestable<T> && Nullable<T> &&
-    requires { typename std::pointer_traits<T>::element_type; };
-
-template <class Alloc>
-concept AllocatorLike = std::is_class_v<Alloc> && requires(Alloc &a,
-    typename std::allocator_traits<Alloc>::size_type n,
-    typename std::allocator_traits<Alloc>::pointer p)
-{
-    a.allocate(n);
-    a.deallocate(p, n);
-};
 
 namespace detail {
 
@@ -60,6 +40,11 @@ inline constexpr bool is_tuple_element<T, 0> = true;
 
 } // namespace detail
 
+/**
+ * Specifies a tuple like type. That is, one which implements the standard
+ * tuple-interface through `std::tuple_size`, `std::tuple_element`, and
+ * `std::get`.
+ */
 template <class T>
 concept TupleLike = requires {
     typename std::tuple_size<T>::type;
@@ -67,30 +52,17 @@ concept TupleLike = requires {
     requires detail::is_tuple_element<T, std::tuple_size_v<T>>;
 };
 
+/**
+ * Specifies a tuple like type constrained to size two, a pair.
+ */
 template <class T>
 concept PairLike = TupleLike<T> && std::tuple_size_v<T> == 2;
 
-template <class T>
-concept Constant = std::is_const_v<std::remove_reference_t<T>>;
-
-template <class T>
-concept Mutable = !Constant<T>;
-
 /**
- * Can be stored (through dynamic allocation). Namely, a cv-unqualified
- * object type. See requirements for allocator value types and contained
- * values.
+ * Specifies a bitmask type, like one used for bitmask flag options.
+ *
+ * @see https://en.cppreference.com/w/cpp/named_req/BitmaskType
  */
-template <class T>
-concept Storable = std::is_object_v<T> && !std::is_const_v<T> &&
-    !std::is_volatile_v<T>;
-
-template <class From, class To>
-concept ExplicitlyConvertibleTo =
-    requires { static_cast<To>(std::declval<From>()); };
-
-
-// https://en.cppreference.com/w/cpp/named_req/BitmaskType
 template <class T>
 concept BitmaskType = requires(T &x, T &y)
 {
@@ -103,7 +75,11 @@ concept BitmaskType = requires(T &x, T &y)
     { x ^= y } -> std::same_as<T &>;
 };
 
-// https://en.cppreference.com/w/cpp/named_req/BasicLockable
+/**
+ * Specifies the basic lockable protocol.
+ *
+ * @see https://en.cppreference.com/w/cpp/named_req/BasicLockable
+ */
 template <class T>
 concept BasicLockable = requires(T &t)
 {
@@ -111,14 +87,22 @@ concept BasicLockable = requires(T &t)
     t.unlock();
 };
 
-// https://en.cppreference.com/w/cpp/named_req/Lockable
+/**
+ * Specifies the general lockable protocol.
+ *
+ * @see https://en.cppreference.com/w/cpp/named_req/Lockable
+ */
 template <class T>
-concept Lockable = requires(T &t)
+concept Lockable = BasicLockable<T> && requires(T &t)
 {
     { t.try_lock() } -> BooleanTestable;
 };
 
-// https://en.cppreference.com/w/cpp/named_req/TimedLockable
+/**
+ * Specifies the timed lockable protocol.
+ *
+ * @see https://en.cppreference.com/w/cpp/named_req/TimedLockable
+ */
 template <class T>
 concept TimedLockable = requires(
     T &t, std::chrono::duration<int> duration,
